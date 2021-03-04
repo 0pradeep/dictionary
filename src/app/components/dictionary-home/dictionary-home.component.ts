834ea8bf91be50ea12e8502f372fd5378c18c193
pradeep <3pradeepj@gmail.com>
2021-03-04 23:13:57 +0530

    Implemented dictionary using angular/dictionary API

--- src/app/components/dictionary-home/dictionary-home.component.ts
+++ src/app/components/dictionary-home/dictionary-home.component.ts
@@ -13,6 +13,8 @@ export class DictionaryHomeComponent implements OnInit {
   wordInfo: WordInfo;
   audio:any;
   disableBtn:boolean = true;
+  recentWords:string[] = [];
+  iError:boolean = false;
 
   constructor(private dictionaryService: DictionaryService) {}
 
@@ -22,11 +24,16 @@ export class DictionaryHomeComponent implements OnInit {
 
   getMeaning(word:string){
     this.dictionaryService.getDictionaryMeaning(word).subscribe(wInfo => {
+      this.iError = false;
       this.wordInfo = wInfo;
       this.audio.src = this.wordInfo[0].phonetics[0].audio;
+      if(!this.recentWords.includes(word)) this.recentWords.unshift(word);
       if(this.audio.src.length > 0) this.disableBtn = false;
-      console.log(this.wordInfo);
+    }, err => {
+      this.iError = true;
     });
+    if(this.recentWords.length > 4) this.recentWords.length = 4;
+    console.log(this.recentWords);
   }
 
   playWordAudio(){
--- src/app/services/dictionary.service.ts
+++ src/app/services/dictionary.service.ts
@@ -19,52 +19,5 @@ export class DictionaryService {
     console.log(requestUrl);
     
     return this.httpClient.get<WordInfo>(requestUrl);
-    // return {
-    //   "word": "dictionary",
-    //   "phonetics": [
-    //     {
-    //       "text": "/ˈdɪkʃəˌnɛri/",
-    //       "audio": "https://lex-audio.useremarkable.com/mp3/dictionary_us_1.mp3"
-    //     }
-    //   ],
-    //   "meanings": [
-    //     {
-    //       "partOfSpeech": "noun",
-    //       "definitions": [
-    //         {
-    //           "definition": "A book or electronic resource that lists the words of a language (typically in alphabetical order) and gives their meaning, or gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage.",
-    //           "example": "I'll look up ‘love’ in the dictionary",
-    //           "synonyms": [
-    //             "lexicon",
-    //             "wordbook",
-    //             "glossary",
-    //             "vocabulary list",
-    //             "vocabulary",
-    //             "word list",
-    //             "wordfinder"
-    //           ]
-    //         }
-    //       ]
-    //     },
-    //     {
-    //       "partOfSpeech": "verb",
-    //       "definitions": [
-    //         {
-    //           "definition": "A book or electronic resource that lists the words of a language (typically in alphabetical order) and gives their meaning, or gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage.",
-    //           "example": "I'll look up ‘love’ in the dictionary",
-    //           "synonyms": [
-    //             "lexicon",
-    //             "wordbook",
-    //             "glossary",
-    //             "vocabulary list",
-    //             "vocabulary",
-    //             "word list",
-    //             "wordfinder"
-    //           ]
-    //         }
-    //       ]
-    //     }
-    //   ]
-    // };
   }
 }
